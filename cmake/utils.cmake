cbc4026cfd9c31dd528ce9d219c8522dd9eecf85
Elie Michel <elie.michel@ens.fr>
2017-10-14 10:57:40 +0200

    Add Qt

---
+++ cmake/utils.cmake
@@ -0,0 +1,10 @@
+
+function(target_link_libraries_and_dll target lib)
+	target_link_libraries(${target} ${lib})
+	add_custom_command(
+	    TARGET piced POST_BUILD
+	    COMMAND ${CMAKE_COMMAND} -E copy_if_different
+	        $<TARGET_FILE:${lib}>
+	        $<TARGET_FILE_DIR:${target}>
+	)
+endfunction()
